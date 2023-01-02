08241f0424e66afe27aae694fc8a0415667dae9b
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2023-01-02 18:05:53 +0200

    Create Input
    Hasn't any logic. Only visual and responsive

--- src/components/Reuse/Input/Input.jsx
+++ src/components/Reuse/Input/Input.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import s from './Input.module.scss';
+
 export const Input = () => {
-  return <></>;
+  return (
+    <div className={s.wrap}>
+      <input className={s.input} type="text" placeholder="Search" />
+      <button className={s.btn}></button>
+    </div>
+  );
 };
