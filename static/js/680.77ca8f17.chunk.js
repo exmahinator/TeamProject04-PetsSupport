426331e9042436b0cbcac3554703693e333c97c2
vchumack <54703870+vchumack@users.noreply.github.com>
2023-01-03 19:18:13 +0000

    Deploying to gh-pages from @ vchumack/petsSupport_front@41789f4bed823b8c9c9494b2312d11599f9ddead 🚀

---
+++ static/js/680.77ca8f17.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpets_support=self.webpackChunkpets_support||[]).push([[680],{9850:function(t,e,n){n.d(e,{D:function(){return u}});n(2791);var r="Title_title__6O6bt",s=n(3329),u=function(t){var e=t.text;return(0,s.jsx)("h2",{className:r,children:e})}},680:function(t,e,n){n.r(e);n(2791);var r=n(9850),s=n(3329);e.default=function(){return(0,s.jsx)("div",{children:(0,s.jsx)(r.D,{text:"Test"})})}}}]);
+//# sourceMappingURL=680.77ca8f17.chunk.js.map
--- static/js/982.feada7de.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpets_support=self.webpackChunkpets_support||[]).push([[982],{982:function(e,s,t){t.r(s);t(2791);var u=t(3329);s.default=function(){return(0,u.jsx)("div",{children:"UserPage"})}}}]);
-//# sourceMappingURL=982.feada7de.chunk.js.map
