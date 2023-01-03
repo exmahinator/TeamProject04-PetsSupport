800bda8f6770981af43fd228575ef409797775b7
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-01-03 16:31:42 +0200

    feature section Notices

--- src/components/Notices/items/NoticesItems.jsx
+++ src/components/Notices/items/NoticesItems.jsx
@@ -1,46 +1,58 @@
 import styles from './NoticesItems.module.scss';
+import {ReactComponent as NoticesHartSvg} from 'shared/images/Notices/Notices-hart.svg';
 
-const posterBg = 'https://via.placeholder.com/350'
-const placeholder = 'https://via.placeholder.com/150'
+const posterBg = 'https://via.placeholder.com/350';
+const placeholder = 'https://via.placeholder.com/150';
 
 const NoticesItems = () => {
-
   return (
-      <div className={styles.wrapper}>
+    <div className={styles.wrapper}>
+      <div className={styles.pctWrap}>
         <img
-          // className={styles.poster}
-          src={posterBg ??  placeholder}
-          alt='pictures dogs'
-          title='title'
+          className={styles.pictures}
+          src={posterBg ?? placeholder}
+          alt="pictures dogs"
+          title="title"
         />
-        <div >
-          <div className={styles.heading}>
-            <h2>Сute dog looking for a home</h2>
-          </div>
-          <div className={styles.paragraph}>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <p className={styles.desc}>Breed: </p>
-                <p>Pomeranian</p>
-              </li>
-              <li className={styles.item}>
-                <p className={styles.desc}>Place:</p>
-                <p>Lviv</p>
-              </li>
-              <li className={styles.item}>
-                <p className={styles.desc}>Age:</p>
-                <span>&nbsp;&nbsp;&nbsp;</span>
-                <p>one year</p>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.button}>
+        <button className={styles.svgWrap}>
+          <NoticesHartSvg/>
+        </button>
+        <div className={styles.categoriesWrap}>
+          <p className={styles.categories}>In good hands</p>
+        </div>
+      </div>
+      <div>
+        <div className={styles.heading}>
+          <h2>Сute dog looking for a home</h2>
+        </div>
+        <div className={styles.paragraph}>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <p className={styles.desc}>Breed: </p>
+              <p>Pomeranian</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.desc}>Place:</p>
+              <p>Lviv</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.desc}>Age:</p>
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <p>one year</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.btnContainer}>
+          <div className={styles.btnWrap}>
             <button>Learn more</button>
+          </div>
+          <div className={styles.btnWrap}>
             <button>Delete</button>
           </div>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default NoticesItems
+export default NoticesItems;
--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './NoticesPage.module.scss';
 import NoticesItems from 'components/Notices/items/NoticesItems'
 import NoticesHeading from 'components/Notices/heading/NoticesHeading';
+import NoticesSearch from 'components/Notices/search/NoticesSearch';
+import NoticesCategories from 'components/Notices/ categories/NoticesCategories';
 
 
 const NoticesPage = () => {
@@ -9,6 +11,17 @@ const NoticesPage = () => {
   return (
     <div className={styles.container}>
       <NoticesHeading/>
+      <NoticesSearch/>
+      <NoticesCategories/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
+      <NoticesItems/>
       <NoticesItems/>
     </div>
   )
