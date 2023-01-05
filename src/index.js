9b94e9a83fdfcc98b4887536aaeaf789a1efc35a
Valeriya Druchinina <vchumack@glmedia.org>
2023-01-05 18:27:36 +0200

    add env var and change BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store, persistor } from './redux/store';
@@ -8,14 +8,16 @@ import { store, persistor } from './redux/store';
 import App from './App';
 import { PersistGate } from 'redux-persist/integration/react';
 
+const { REACT_APP_BASE_URL } = process.env;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<HashRouter>
+			<BrowserRouter basename={REACT_APP_BASE_URL}>
 				<App />
-			</HashRouter>
+			</BrowserRouter>
 		</PersistGate>
 	</Provider>
 
