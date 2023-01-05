cd663bbe9b2082126731d2b552760acf362e5867
Valeriya Druchinina <vchumack@glmedia.org>
2023-01-05 20:27:26 +0200

    fix netlify build

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import { store, persistor } from './redux/store';
 import App from './App';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const { REACT_APP_BASE_URL } = process.env;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter basename={REACT_APP_BASE_URL}>
+			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</PersistGate>
