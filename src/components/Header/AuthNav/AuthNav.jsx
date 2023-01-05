666deda4908007697ad50e81153a51c60e2948d0
Demchenk0 <santadyr2016@gmail.com>
2023-01-05 13:58:04 +0200

    Header

--- src/components/Header/AuthNav/AuthNav.jsx
+++ src/components/Header/AuthNav/AuthNav.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import style from './AuthNav.module.scss';
 
 export const AuthNav = () => {
-  return (
-    <div className={style.btn}>
-      <div className={style.login}>
-        <Link className={style.linkLogo}>Login</Link>
-      </div>
-      <div className={style.registration}>
-        <Link className={style.linkRegistration}>Registration</Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.btn}>
+			<div className={style.login}>
+				<Link className={style.linkLogo}>Login</Link>
+			</div>
+			<div className={style.registration}>
+				<Link className={style.linkRegistration}>Registration</Link>
+			</div>
+		</div>
+	);
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom/dist';
 import { Header } from './../Header/Header';
+// import style from './SharedLayout.module.scss';
 
 export const SharedLayout = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
 };
