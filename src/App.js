ae38c852cc3f166e661440f07922dc760896fa6b
Valeriya Druchinina <vchumack@glmedia.org>
2023-01-05 23:48:25 +0200

    add redux services and fix bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { lazy, Suspense, useMemo } from 'react';
-import { useDispatch } from 'react-redux';
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { current } from 'redux/auth/auth-operations';
+import { getToken } from 'redux/auth/auth-selectors';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
 
 import './shared/styles/style.scss';
@@ -18,7 +19,14 @@ const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 
 function App() {
 	const dispatch = useDispatch();
-	useMemo(() => dispatch(current()), [dispatch]);
+	const token = useSelector(getToken);
+
+	useEffect(() => {
+		if (token) {
+			dispatch(current());
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<Suspense fallback={<p>....Load page</p>}>
