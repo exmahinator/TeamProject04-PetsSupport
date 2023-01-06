2106054780db4e5440003771ef83cd3a55e91aab
Demchenk0 <santadyr2016@gmail.com>
2023-01-06 13:22:49 +0200

    linkNav

--- src/components/Header/UserNav/UserNav.jsx
+++ src/components/Header/UserNav/UserNav.jsx
@@ -4,14 +4,16 @@ import style from './UserNav.module.scss';
 import { ReactComponent as AccountCircle } from 'shared/images/AccountCircle/AccountCircle.svg';
 
 export const UserNav = () => {
-  return (
-    <div className={style.container}>
-      <AccountCircle
-        className={style.svg}
-        width="28px"
-        heigth="28px"
-      ></AccountCircle>
-      <Link className={style.link}>Account</Link>
-    </div>
-  );
+	return (
+		<div className={style.container}>
+			<AccountCircle
+				className={style.svg}
+				width="28px"
+				heigth="28px"
+			></AccountCircle>
+			<Link to="/user" className={style.link}>
+				Account
+			</Link>
+		</div>
+	);
 };
