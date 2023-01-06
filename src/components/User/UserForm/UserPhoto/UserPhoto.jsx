28a46e311eb27de62414eabc66bf885b772b213a
Pavlo Vovzhynskyy <pavlovovzhynskyy@MacBook-Air-Pavlo.local>
2023-01-06 18:36:38 +0100

    faced bug, need help

--- src/components/User/UserForm/UserPhoto/UserPhoto.jsx
+++ src/components/User/UserForm/UserPhoto/UserPhoto.jsx
@@ -5,53 +5,66 @@ import defaultAvatar from 'shared/images/user/anonymous.png';
 import style from '../UserForm.module.scss';
 
 export const UserPhoto = ({
-	avtar,
+	avatar,
+	watch,
 	register,
 	uploadClicked,
 	onUploadClick,
 	onUploadSubmit,
 }) => {
-	console.log('uploadClicked', uploadClicked);
+	// console.log('ava',watch(({ avatar }) => avatar.length === 1));
+
+	// console.log(avatar);
 	return (
 		<>
 			<div className={style.avatar__thumb}>
 				<div className={style.thumb}>
 					<img
 						className={style.user__avatar}
-						src={avtar ? avtar : defaultAvatar}
+						src={avatar ? avatar : defaultAvatar}
 						alt="default avatar"
 						width={233}
 						height={233}
 					/>
 				</div>
 				<div className={style.user__uploadThumb}>
-					<button type="submit" onSubmit={onUploadSubmit}>
-						<label htmlFor="avatarSubmit" className={style.user__uploadLabel}>
-							<UploadSubmit fill="#F59256" width={18} height={18} />
-							<p className={style.user__uploadText}>submit</p>
-						</label>
-						<input
-							type="file"
-							name="upload"
-							{...register('avatar')}
-							id="avatarSubmit"
-							className={style.user__uploadInput}
-						/>
-					</button>
+					{!avatar ? (
+						<button type="submit" onSubmit={onUploadSubmit}>
+							<label htmlFor="avatarSubmit" className={style.user__uploadLabel}>
+								<UploadSubmit fill="#F59256" width={18} height={18} />
+								<p className={style.user__uploadText}>submit</p>
+							</label>
+							<input
+								type="file"
+								name="upload"
+								{...register('avatar')}
+								id="avatarSubmit"
+								className={style.user__uploadInput}
+							/>
+						</button>
+					) : (
+						<>
+							<label
+								htmlFor="avatar"
+								className={style.user__uploadLabel}
+								onClick={onUploadClick}
+							>
+								<UploadIcon fill="#F59256" width={18} height={18} />
+								<p className={style.user__uploadText}>edit Photo</p>
+							</label>
+							<input
+								type="file"
+								name="avatar"
+								{...register('avatar')}
+								id="avatar"
+								className={style.user__uploadInput}
+							/>
+						</>
+					)}
 
-					<button type="button" onClick={onUploadClick}>
-						<label htmlFor="avatar" className={style.user__uploadLabel}>
-							<UploadIcon fill="#F59256" width={18} height={18} />
-							<p className={style.user__uploadText}>edit Photo</p>
-						</label>
-						<input
-							type="file"
-							name="avatar"
-							{...register('avatar')}
-							id="avatar"
-							className={style.user__uploadInput}
-						/>
-					</button>
+					{/* <button type="button"> */}
+
+					{/* </button> */}
 				</div>
 			</div>
 		</>
