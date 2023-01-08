4e1826e99eb1c4074fc39d619b95525f38818535
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-01-09 01:50:33 +0200

    merged main

--- src/components/User/AddPetForm/AddPetForm.jsx
+++ src/components/User/AddPetForm/AddPetForm.jsx
@@ -41,8 +41,7 @@ export const AddPetForm = ({ onCloseModal }) => {
 			setUploadError(false)
 			newPet.append('avatar', petUpload[0]);
 		}
-
-
+		
 		dispatch(addUserPet(newPet));
 
 		onCloseModal();
