2f88dd05b0b6e70f2e2a8e1cf2d5999360448739
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-01-08 18:35:06 +0200

    added styles sections Notices Categoties

--- src/components/Notices/categories/NoticesCategories.jsx
+++ src/components/Notices/categories/NoticesCategories.jsx
@@ -1,30 +1,61 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'shared/hooks/useAuth';
-import styles from './NoticesCategories.module.scss';
+import style from './NoticesCategories.module.scss';
 
 const NoticesCategories = () => {
+	const activ = { color: '#FFFFFF', background: '#ff6101' };
+	const notActive = { color: '#111111'};
 
-
-	const isLogin = useAuth()
+	const isLogin = useAuth();
 
 	return (
-		<ul className={styles.wrapper}>
-			<li>
-				<NavLink to="/notices/sell">sell</NavLink>
+		<ul className={style.nav}>
+			<li className={style.nav__item}>
+				<NavLink
+					to="/notices/sell"
+					className={style.nav__link}
+					style={({ isActive }) => (isActive ? activ : notActive)}
+				>
+					sell
+				</NavLink>
 			</li>
-			<li>
-				<NavLink to="/notices/lost-found">lost/found</NavLink>
+			<li className={style.nav__item}>
+				<NavLink
+					to="/notices/lost-found"
+					className={style.nav__link}
+					style={({ isActive }) => (isActive ? activ : notActive)}
+				>
+					lost/found
+				</NavLink>
 			</li>
-			<li>
-				<NavLink to="/notices/for-free">in good hands</NavLink>
+			<li className={style.nav__item}>
+				<NavLink
+					to="/notices/for-free"
+					className={style.nav__link}
+					style={({ isActive }) => (isActive ? activ : notActive)}
+				>
+					in good hands
+				</NavLink>
 			</li>
 			{isLogin && (
 				<>
-					<li>
-						<NavLink to="/notices/favorite">favorite</NavLink>
+					<li className={style.nav__item}>
+						<NavLink
+							to="/notices/favorite"
+							className={style.nav__link}
+							style={({ isActive }) => (isActive ? activ : notActive)}
+						>
+							favorite
+						</NavLink>
 					</li>
-					<li>
-						<NavLink to="/notices/own">my ads</NavLink>
+					<li className={style.nav__item}>
+						<NavLink
+							to="/notices/own"
+							className={style.nav__link}
+							style={({ isActive }) => (isActive ? activ : notActive)}
+						>
+							my ads
+						</NavLink>
 					</li>
 				</>
 			)}
