002b275f6cb34428d1d4dec9572fd17cff3b0d38
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-01-09 11:57:43 +0200

    add news heading tooltip

--- src/components/News/NewsHeading/NewsHeading.jsx
+++ src/components/News/NewsHeading/NewsHeading.jsx
@@ -1,6 +1,9 @@
 import styles from './NewsHeading.module.scss'
 export const NewsHeading = ({ children }) => {
     return (
+        <>
         <h3 className={styles.heading}>{children}</h3>
+        <p className={styles.tooltip}>{children}</p>
+        </>
     )
 }
