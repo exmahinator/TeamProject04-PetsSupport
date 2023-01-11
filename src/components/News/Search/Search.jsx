0b9b443c46e1a4cdddcd721757dd258edd6a0582
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-01-11 15:55:07 +0200

    added a tooltip to the search on news page

--- src/components/News/Search/Search.jsx
+++ src/components/News/Search/Search.jsx
@@ -1,47 +1,42 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useState } from 'react';
 import { ReactComponent as SearchIcon } from 'shared/images/overused/NewsSearch.svg';
 import { ReactComponent as ClearIcon } from 'shared/images/overused/x-circle.svg';
 
-import debounce from 'lodash.debounce';
+
 import style from './Search.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { getQueryParams } from 'redux/news/news-selectors';
 import { setQueryParams } from 'redux/news/news-slice';
 
 export const Search = () => {
-	const { query } = useSelector(getQueryParams);
+	const {query} = useSelector(getQueryParams);
+	const [isCloseIcon, setIsCloseIcon] = useState(!!query)
 
 	const dispatch = useDispatch();
 	const input = useRef();
 	if (input.current) input.current.value = query ?? '';
 
-	const clearBtnHandler = () => {
-		input.current.value = '';
-		dispatch(setQueryParams({}));
+	const btnHandler = () => {
+		setIsCloseIcon(prev => !prev)
+		if (isCloseIcon) {
+			dispatch(setQueryParams({ query: '' }));
+			return;
+		}
+		dispatch(setQueryParams({query: input.current.value}));
 	};
-
-	const changeHandler = ({ target: { value } }) => {
-		dispatch(setQueryParams({ query: value }));
-	};
-	// eslint-disable-next-line
-	const debouncedChangeHandler = useCallback(debounce(changeHandler, 700), []);
 
 	return (
 		<div className={style.wrapper}>
 			<input
 				ref={input}
-				onChange={debouncedChangeHandler}
 				className={style.input}
 				type="text"
 				placeholder="Search"
-			/>
-			{input.current?.value ? (
-				<button className={style.btn} onClick={clearBtnHandler}>
-					<ClearIcon className={style.clear} />
-				</button>
-			) : (
-				<SearchIcon className={style.search} />
-			)}
+			/>		
+			<p className={style.tooltip}>Enter the search word and press <SearchIcon className={style.searchTooltip} /></p>
+				<button className={style.btn} onClick={btnHandler}>
+					{isCloseIcon ? <ClearIcon className={style.clear} />:<SearchIcon className={style.search} /> }
+				</button>		
 		</div>
 	);
 };
