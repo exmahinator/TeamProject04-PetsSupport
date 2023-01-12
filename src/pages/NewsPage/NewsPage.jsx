31050249f8b4b998f27baa81f6d418bbaf7becbc
Valeriya Druchinina <vchumack@glmedia.org>
2023-01-12 19:30:12 +0200

    added spinner

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -4,44 +4,50 @@ import { NewsList } from 'components/News/NewList/NewsList';
 import { Container } from 'components/Reuse/Container/Container';
 import { Title } from 'components/Reuse/Title/Title';
 
-import {  useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  getNews, getQueryParams, getTotalPages, getNewsLoading,
-  // getNewsError,
-} from '../../redux/news/news-selectors'
-import { getAllNews } from '../../redux/news/news-operations'
+	getNews,
+	getQueryParams,
+	getTotalPages,
+	getNewsLoading,
+	// getNewsError,
+} from '../../redux/news/news-selectors';
+import { getAllNews } from '../../redux/news/news-operations';
 import { Search } from 'components/News/Search/Search';
 import { PaginationList } from 'components/Reuse/Pagination/PaginationList/PaginationList';
 import { NewsEmpty } from 'components/News/NewsEmpty/NewsEmpty';
-
+import { BigSpinner } from 'components/Reuse/Loaders/Spinner/BigSpinner';
 
 const NewsPage = () => {
+	const dispatch = useDispatch();
+	const news = useSelector(getNews);
+	const totalPages = useSelector(getTotalPages);
+	const queryParams = useSelector(getQueryParams);
+	const isLoading = useSelector(getNewsLoading);
 
-  const dispatch = useDispatch()
-  const news = useSelector(getNews)
-  const totalPages = useSelector(getTotalPages)
-  const queryParams = useSelector(getQueryParams)
-  const isLoading = useSelector(getNewsLoading)
+	const noResults = !news.length && !isLoading;
 
-  const noResults = !news.length && !isLoading
+	useEffect(() => {
+		dispatch(getAllNews(queryParams));
+	}, [dispatch, queryParams]);
 
-  useEffect(() => {
-    dispatch(getAllNews(queryParams))
-     },[dispatch, queryParams])
+	return (
+		<section className={styles.section}>
+			<Container>
+				<Title text="News" />
+				<Search />
+				{isLoading && (
+					<div className={styles.spinner}>
+						<BigSpinner />
+					</div>
+				)}
+				{noResults && <NewsEmpty />}
+				<NewsList data={news} />
+				{totalPages > 1 && <PaginationList pages={totalPages} />}
+			</Container>
+		</section>
+	);
+};
 
-
-  return (
-    <section className={styles.section}>
-      <Container>
-      <Title text='News' />
-        <Search />   
-        {noResults && <NewsEmpty/>}
-        <NewsList data={news} />
-        {totalPages > 1 && <PaginationList pages={ totalPages} />}
-      </Container>
-    </section>
-  )
-}
-
-export default NewsPage
+export default NewsPage;
--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getServices } from '../../redux/services/services-selectors';
+import {
+	getServices,
+	getServicesLoading,
+} from '../../redux/services/services-selectors';
 import { getAllServices } from '../../redux/services/services-operations';
 import { Container } from '@mui/material';
 import { Title } from 'components/Reuse/Title/Title';
 import { OurFriendsList } from './OurFriendsList/OurFriendsList';
 import style from './OurFriendsPage.module.scss';
+import { BigSpinner } from 'components/Reuse/Loaders/Spinner/BigSpinner';
 
 const OurFriendsPage = () => {
 	const dispatch = useDispatch();
 	const services = useSelector(getServices);
+	const isLoading = useSelector(getServicesLoading);
 
 	useEffect(() => {
 		dispatch(getAllServices());
@@ -19,6 +24,11 @@ const OurFriendsPage = () => {
 		<section className={style.friends_section}>
 			<Container>
 				<Title text={'Our friends'} />
+				{isLoading && (
+					<div className={style.spinner}>
+						<BigSpinner />
+					</div>
+				)}
 				<OurFriendsList data={services} />
 			</Container>
 		</section>
