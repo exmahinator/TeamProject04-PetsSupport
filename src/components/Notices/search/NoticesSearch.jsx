8fbb7f1c14031e6faccbb9c250177dec8c1fdfe6
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-01-13 12:52:28 +0200

    fixed warning

--- src/components/Notices/search/NoticesSearch.jsx
+++ src/components/Notices/search/NoticesSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {   setQueryParams } from 'redux/notices/notices-slice';
 import style from './NoticesSearch.module.scss';
